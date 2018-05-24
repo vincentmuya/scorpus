41dc3658f91e5a7ac72dc2009d1ab481d2111e50
vincentmuya <vincentmuya13@gmail.com>
2018-05-24 18:16:32 +0300

    modified max_length and removed number

--- scoty/models.py
+++ scoty/models.py
@@ -1,17 +1,17 @@
 from django.db import models
 
 class NewForm(models.Model):
-    SenderName=models.CharField(max_length = 30)
-    SenderAddress=models.CharField(max_length = 30)
-    RecieverName=models.CharField(max_length = 30)
-    RecieverAddress=models.CharField(max_length = 30)
-    referenceID=models.CharField(max_length = 30)
-    GoodsareFrom=models.CharField(max_length = 30)
-    GoodsTo=models.CharField(max_length = 30)
-    DepatureDate=models.CharField(max_length = 30)
-    Depaturetime=models.CharField(max_length = 30)
-    ArrivalDate=models.CharField(max_length = 30)
-    ArrivalTime=models.CharField(max_length = 30)
+    SenderName=models.CharField(max_length = 250)
+    SenderAddress=models.CharField(max_length = 250)
+    RecieverName=models.CharField(max_length = 250)
+    RecieverAddress=models.CharField(max_length = 250)
+    referenceID=models.CharField(max_length = 250)
+    GoodsareFrom=models.CharField(max_length = 250)
+    GoodsTo=models.CharField(max_length = 250)
+    DepatureDate=models.CharField(max_length = 250)
+    Depaturetime=models.CharField(max_length = 250)
+    ArrivalDate=models.CharField(max_length = 250)
+    ArrivalTime=models.CharField(max_length = 250)
     GoodsDescription=models.TextField(max_length = 1000)
     Status=models.TextField(max_length = 5000)
 
